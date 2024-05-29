9cbd32594dbd7d297e905529ab4fe4e6908b6ed7
Vasudev Patidar <169156117+Vasu123216@users.noreply.github.com>
2024-05-29 16:26:48 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+      rel="stylesheet">
+    <link rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Welcome</title>
+</head>
+<body>
+    <nav class="navbar navbar">
+        <div class="container-fluid bg-dark">
+            <a class="navbar-brand text-white" href="login.php">Student CRUD System</a>
+        </div>
+    </nav>
+    <div class="container bg-light p-3">
+        <h1 class="text-center">Welcome to the student CRUD System</h1>
+        <table class="table table-striped table-bordered p-3">
+            <thead>
+                <tr>
+                    <th>Registered User?</th>
+                    <th>New User!</th>
+                </tr>
+            </thead>
+            <tr>
+                <th>
+                    <div class="p-3">
+                        <a href="login.php"><button class="btn bg-primary text-white ">LogIn</button></a>
+                    </div>
+                </th>
+                <th>
+                    <div class="p-3" >
+                        <a href="register.php"><button class="btn bg-primary text-white">SignUp</button></a>
+                    </div>
+                </th>
+            </tr>
+        </table>
+    </div>
+    </div>
+</body>
+</html>
+
